c47483ce18cca19ea7f6c4a5286208e4e0df2d89
NorbertVarga <varga.n0124@gmail.com>
2022-10-06 15:01:40 +0200

    Error files

--- src/main/java/com/NorbertVarga/SpringBootSecuritydemoProject/errorHandling/GlobalExceptionHandler.java
+++ src/main/java/com/NorbertVarga/SpringBootSecuritydemoProject/errorHandling/GlobalExceptionHandler.java
@@ -1,4 +1,62 @@
 package com.NorbertVarga.SpringBootSecuritydemoProject.errorHandling;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Locale;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private final MessageSource messageSource;
+
+    @Autowired
+    public GlobalExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    // Validation errors throws "MethodArgumentNotValidException"
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ValidationError> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        logger.error("A validation error occurred: ", ex);
+        BindingResult result = ex.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        return new ResponseEntity<>(processFieldErrors(fieldErrors), HttpStatus.BAD_REQUEST);
+    }
+
+    private ValidationError processFieldErrors(List<FieldError> fieldErrors) {
+        ValidationError validationError = new ValidationError();
+
+        for (FieldError fieldError: fieldErrors) {
+            validationError.addFieldError(fieldError.getField(), messageSource.getMessage(fieldError, Locale.getDefault()));
+        }
+
+        return validationError;
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiError> handleEntityNotFoundException(EntityNotFoundException ex) {
+        logger.error("User principal error: ", ex);
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        ApiError body = new ApiError(
+                "NOT_FOUND",
+                "En entity is missing for the operation",
+                ex.getLocalizedMessage());
+
+        return new ResponseEntity<>(body, status);
+    }
 }
