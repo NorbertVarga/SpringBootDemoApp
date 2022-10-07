a4198b4ca536d88176eaa33aad39771c6ba6c84b
NorbertVarga <varga.n0124@gmail.com>
2022-10-07 15:03:07 +0200

    Purchase and productOrderitem structure added

--- src/main/java/com/NorbertVarga/SpringBootSecuritydemoProject/entity/purchase/ProductOrder.java
+++ src/main/java/com/NorbertVarga/SpringBootSecuritydemoProject/entity/purchase/ProductOrder.java
@@ -1,4 +1,83 @@
 package com.NorbertVarga.SpringBootSecuritydemoProject.entity.purchase;
 
+import com.NorbertVarga.SpringBootSecuritydemoProject.entity.product.Product;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+// Entity Listener needed for auditing and automatically manage the creating and modified dates
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Table (name = "product_order")
 public class ProductOrder {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "product_order_id")
+    private Long productOrderId;
+
+    @OneToOne
+    @JoinColumn (name = "product_id")
+    private Product product;
+
+    @Column (name = "product_order_quantity")
+    private int quantity;
+
+    @Column(name = "product_order_total_price")
+    private int totalPrice;
+
+    @Column(name = "product_order_created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public ProductOrder() {
+    }
+
+    public ProductOrder(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+        this.totalPrice = product.getPrice() * quantity;
+    }
+
+    public Long getProductOrderId() {
+        return productOrderId;
+    }
+
+    public void setProductOrderId(Long productOrderId) {
+        this.productOrderId = productOrderId;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
--- src/main/java/com/NorbertVarga/SpringBootSecuritydemoProject/entity/purchase/PurchaseItem.java
+++ src/main/java/com/NorbertVarga/SpringBootSecuritydemoProject/entity/purchase/PurchaseItem.java
@@ -1,4 +1,86 @@
 package com.NorbertVarga.SpringBootSecuritydemoProject.entity.purchase;
 
+import com.NorbertVarga.SpringBootSecuritydemoProject.entity.userAccount.UserAccount;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Entity
+@Table
+// Entity Listener needed for auditing and automatically manage the creating and modified dates
+@EntityListeners(AuditingEntityListener.class)
 public class PurchaseItem {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "purchase_item_id")
+    private Long purchaseItemId;
+
+    @ManyToOne // todo update userAccount entity and related DTO
+    @JoinColumn (name = "user_id")
+    private UserAccount userAccount;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "purchase_item_id")
+    private List<ProductOrder> productOrderList;
+
+    @Column(name = "purchase_item_total_price")
+    private int totalPrice;
+
+    @Column(name = "purchase_item_created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public PurchaseItem() {
+    }
+
+    public PurchaseItem(UserAccount userAccount, List<ProductOrder> productOrderList) {
+        this.userAccount = userAccount;
+        this.productOrderList = productOrderList;
+        this.totalPrice = productOrderList.stream().mapToInt(ProductOrder::getTotalPrice).sum();
+    }
+
+    public Long getPurchaseItemId() {
+        return purchaseItemId;
+    }
+
+    public void setPurchaseItemId(Long purchaseItemId) {
+        this.purchaseItemId = purchaseItemId;
+    }
+
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    public List<ProductOrder> getProductOrderList() {
+        return productOrderList;
+    }
+
+    public void setProductOrderList(List<ProductOrder> productOrderList) {
+        this.productOrderList = productOrderList;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
