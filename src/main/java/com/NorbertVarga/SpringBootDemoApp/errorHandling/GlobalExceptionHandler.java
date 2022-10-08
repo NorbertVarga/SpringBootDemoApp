201e4ca7d8729705ab7d05c24ede0c0afdc7b65e
NorbertVarga <varga.n0124@gmail.com>
2022-10-09 01:44:52 +0200

    HttpMessageNotReadableException added to GlobalExceptionHandler

--- src/main/java/com/NorbertVarga/SpringBootDemoApp/errorHandling/GlobalExceptionHandler.java
+++ src/main/java/com/NorbertVarga/SpringBootDemoApp/errorHandling/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -30,7 +31,7 @@ public class GlobalExceptionHandler {
         this.messageSource = messageSource;
     }
 
-    // Validation errors throws "MethodArgumentNotValidException"
+    //  **  Validation errors and JSON related  //////////////////////////////
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected ResponseEntity<ValidationError> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         logger.error("A validation error occurred: ", ex);
@@ -39,10 +40,23 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(processFieldErrors(fieldErrors), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ApiError> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        logger.error("JSON PARSE ERROR: ", ex);
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ApiError body = new ApiError(
+                "BAD_REQUEST",
+                "Ups, something went wrong...",
+                "Sorry, we can't tell you what was the problem.");
+        return new ResponseEntity<>(body, status);
+    }
+    /////////////////////////////////////////////////////////
+
+    //  **  DATABASE   ////////////////////////////////
     // These exceptions related to Hibernate Entity level validation, or jpa, or jdbc
     // (Data cannot be saved to the database)
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ApiError> handleConstraintViolationException(ConstraintViolationException ex) {
+    protected ResponseEntity<ApiError> handleConstraintViolationException(ConstraintViolationException ex) {
         logger.error("Database validation error: ", ex);
         HttpStatus status = HttpStatus.BAD_REQUEST;
         ApiError body = new ApiError(
@@ -53,7 +67,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(JdbcSQLDataException.class)
-    public ResponseEntity<ApiError> handleJdbcSQLDataException(JdbcSQLDataException ex) {
+    protected ResponseEntity<ApiError> handleJdbcSQLDataException(JdbcSQLDataException ex) {
         logger.error("Database validation error: ", ex);
         HttpStatus status = HttpStatus.BAD_REQUEST;
         ApiError body = new ApiError(
@@ -62,10 +76,11 @@ public class GlobalExceptionHandler {
                 "Sorry, we can't tell you what was the problem.");
         return new ResponseEntity<>(body, status);
     }
-    //////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
 
+    //  **  BUSINESS   ////////////////////
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ApiError> handleEntityNotFoundException(EntityNotFoundException ex) {
+    protected ResponseEntity<ApiError> handleEntityNotFoundException(EntityNotFoundException ex) {
         logger.error("User principal error: ", ex);
         HttpStatus status = HttpStatus.NOT_FOUND;
         ApiError body = new ApiError(
@@ -76,7 +91,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(EntityExistsException.class)
-    public ResponseEntity<ApiError> handleEntityExistException(EntityExistsException ex) {
+    protected ResponseEntity<ApiError> handleEntityExistException(EntityExistsException ex) {
         logger.error("User principal error: ", ex);
         HttpStatus status = HttpStatus.BAD_REQUEST;
         ApiError body = new ApiError(
@@ -85,8 +100,9 @@ public class GlobalExceptionHandler {
                 ex.getLocalizedMessage());
         return new ResponseEntity<>(body, status);
     }
+    ////////////////////////////////////////////////////////////
 
-
+    //  **  UTILS  ////////////////////////////////
     private ValidationError processFieldErrors(List<FieldError> fieldErrors) {
         ValidationError validationError = new ValidationError();
         for (FieldError fieldError : fieldErrors) {
