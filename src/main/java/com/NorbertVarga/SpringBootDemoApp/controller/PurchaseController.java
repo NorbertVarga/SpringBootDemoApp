e5907ef1a5c03dac6d3d6ff95dea5489c81f0aa8
A200133714 <varga.n0124@gmail.com>
2022-10-12 12:13:18 +0200

    PurchaseController makePurchase & myPurchases endpoint implement

--- src/main/java/com/NorbertVarga/SpringBootDemoApp/controller/PurchaseController.java
+++ src/main/java/com/NorbertVarga/SpringBootDemoApp/controller/PurchaseController.java
@@ -1,9 +1,16 @@
 package com.NorbertVarga.SpringBootDemoApp.controller;
 
+import com.NorbertVarga.SpringBootDemoApp.dto.purchase_cart.PurchaseItemData_DTO;
 import com.NorbertVarga.SpringBootDemoApp.service.PurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/purchase")
@@ -15,5 +22,19 @@ public class PurchaseController {
     @Autowired
     public PurchaseController(PurchaseService purchaseService) {
         this.purchaseService = purchaseService;
+    }
+
+    @Secured({"ROLE_USER", "ROLE_ADMIN"})
+    @GetMapping("/make-purchase")
+    public ResponseEntity<PurchaseItemData_DTO> makePurchase() {
+        PurchaseItemData_DTO purchaseData = purchaseService.makePurchase();
+        return new ResponseEntity<>(purchaseData, HttpStatus.OK);
+    }
+
+    @Secured({"ROLE_USER", "ROLE_ADMIN"})
+    @GetMapping("/my-purchases")
+    public ResponseEntity<List<PurchaseItemData_DTO>> getMyPurchases() {
+        List<PurchaseItemData_DTO> myPurchasesData = purchaseService.getMyPurchases();
+        return new ResponseEntity<>(myPurchasesData, HttpStatus.OK);
     }
 }
