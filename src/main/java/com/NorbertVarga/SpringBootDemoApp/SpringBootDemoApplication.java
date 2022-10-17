8b5936aee4a188ba6d1943e59947cddf12c8d3e7
A200133714 <varga.n0124@gmail.com>
2022-10-17 11:44:38 +0200

    renaming main class

--- src/main/java/com/NorbertVarga/SpringBootDemoApp/SpringBootDemoApplication.java
+++ src/main/java/com/NorbertVarga/SpringBootDemoApp/SpringBootDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSecurityDemoProjectApplication {
+public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityDemoProjectApplication.class, args);
+		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
 }
