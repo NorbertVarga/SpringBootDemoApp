2fd9736c441fc7107ea2a86cab62a78beb2c4152
A200133714 <varga.n0124@gmail.com>
2022-10-24 14:13:07 +0200

    logging is working

--- src/main/java/com/NorbertVarga/SpringBootDemoApp/SpringBootDemoApplication.java
+++ src/main/java/com/NorbertVarga/SpringBootDemoApp/SpringBootDemoApplication.java
@@ -1,13 +1,19 @@
 package com.NorbertVarga.SpringBootDemoApp;
 
+import com.NorbertVarga.SpringBootDemoApp.errorHandling.GlobalExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(SpringBootDemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		logger.trace("** APPLICATION STARTUP **");
 	}
 
 }
