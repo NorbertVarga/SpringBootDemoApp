cceff18b7889cc2b19f6967fe0e9f5842752f67d
A200133714 <varga.n0124@gmail.com>
2022-10-25 13:55:30 +0200

    logging controller aspect implement

--- src/main/java/com/NorbertVarga/SpringBootDemoApp/config/loggingAspects/ControllerAspect.java
+++ src/main/java/com/NorbertVarga/SpringBootDemoApp/config/loggingAspects/ControllerAspect.java
@@ -1,17 +1,22 @@
 package com.NorbertVarga.SpringBootDemoApp.config.loggingAspects;
 
+import com.NorbertVarga.SpringBootDemoApp.entity.userAccount.UserPrincipal;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @Aspect
 @Component
@@ -22,19 +27,25 @@ public class ControllerAspect {
     @Around("execution(* com.NorbertVarga.SpringBootDemoApp.controller..*(..))")
     public Object profileAllMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable
     {
-        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest();
         String endpoint = request.getRequestURI();
-        //Get intercepted method details
-        String className = methodSignature.getDeclaringType().getSimpleName();
-        String methodName = methodSignature.getName();
+        String ipAddress = request.getRemoteAddr();
 
-        Object result = proceedingJoinPoint.proceed();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userEmail;
+        String userFullName;
 
-        //Log method execution time
-        LOGGER.info("Controller called: " + endpoint + " method:" + methodName);
+        if (auth.getPrincipal() != "anonymousUser") {
+            UserPrincipal principal = (UserPrincipal) auth.getPrincipal();
+            userEmail = principal.getEmail();
+            userFullName = principal.getFirstName() + " " + principal.getLastName();
+            LOGGER.info("** Controller called: " + endpoint + " FROM: " + userEmail + " " + userFullName + " " + ipAddress);
+        } else {
+            LOGGER.info("** Controller called by guest: " + endpoint + " FROM: " + ipAddress);
+        }
 
-        return result;
+
+        return proceedingJoinPoint.proceed();
     }
 }
