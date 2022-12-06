4e28ee87c279cc861eded7cef47fb9eba38c59c6
A200133714 <varga.n0124@gmail.com>
2022-12-06 14:11:25 +0100

    register component form implement

--- webshopFE/webshop-demo/src/app/app-routing.module.ts
+++ webshopFE/webshop-demo/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
+import {ProductComponent} from "./component/product/product.component";
+import {LoginComponent} from "./component/login/login.component";
+import {UserProfileComponent} from "./component/user-profile/user-profile.component";
+import {RegisterComponent} from "./component/register/register.component";
+import {AdminComponent} from "./component/admin/admin.component";
 
 const routes: Routes = [
-
+  { path: '', component: HomeComponent},
+  { path: 'products', component: ProductComponent},
+  { path: 'my-profile', component: UserProfileComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'admin', component: AdminComponent}
 ];
 
 @NgModule({
